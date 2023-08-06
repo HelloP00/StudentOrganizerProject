5ad6cb6970e2c8d349df29dc597f0744e84726c9
0617300 <>
2023-08-06 12:03:12 +1000

    added test file for storing passwords
    This is stored in the folder "CryptographyTest" and is a dotnet project.

---
+++ CryptographyTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+//requires a usernername input to run. 
+
+
+
+void GenKeys(string userName) {
+	string saveDir = $".\\{userName}";
+	
+	byte[] key, iv;
+	
+	//makes the keys
+	using (Aes aes = Aes.Create()) {
+		key = aes.Key;
+		iv = aes.IV;
+	}
+	Console.WriteLine("Keys generated, attempting to store...");
+	
+	//stores the keys
+	//using (FileStream stream = File.OpenOrCreate($"{saveDir}")) {
+		using (StreamWriter writer = new StreamWriter($"{saveDir}\\key.txt")) {
+			writer.Write(key);
+		}
+	//}
+	//using (FileStream stream = File.OpenOrCreate($"{saveDir}")) {
+		using (StreamWriter writer = new StreamWriter($"{saveDir}\\IV.txt")) {
+			writer.Write(iv);
+		}
+	//}
+}
+
+
+GenKeys("testUser");
