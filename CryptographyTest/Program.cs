f6a482e8179bed6526ce5d94e6718f2a4f0ac726
0617300 <>
2023-08-06 13:10:52 +1000

    loading in the keys
    The function "HashPassword" can load the keys stored through 'GenKeys'

--- CryptographyTest/Program.cs
+++ CryptographyTest/Program.cs
@@ -33,23 +33,48 @@ void GenKeys(string userName) {
 	}
 }
 
-/*
+
 //hashes the string to the keys stored under the username
-void HashPassword(string userName) {
+void HashPassword(string userName, string inputPassword) {
 	string userDir = $".\\{userName}";
+	int keySize = 32, IVSize = 16;
 	
-	byte[] key, iv;
+	byte[] key = new byte[keySize], iv = new byte[IVSize];
 	
-	using (Aes aes = Aes.Create) {
+	using (Aes aes = Aes.Create()) {
 		
-		
-		using (FileStream stream = File.Open($"{userDir}\\key.dat")) {
+		//loading the keys
+		using (FileStream stream = File.Open($"{userDir}\\key.dat", FileMode.Open)) {
 			using (BinaryReader reader = new BinaryReader(stream)) {
-				
+				reader.Read(key, 0, keySize);
+				aes.Key = key;
+			}
+		}
+		
+		//ripped from GenKeys to confirm if it gets the info right
+		using (FileStream stream = File.Create($"{userDir}\\randIV.dat")) {
+			using (BinaryWriter writer = new BinaryWriter(stream)) {
+				writer.Write(aes.IV);
+			}
+		}
+		
+		using (FileStream stream = File.Open($"{userDir}\\IV.dat", FileMode.Open)) {
+			using (BinaryReader reader = new BinaryReader(stream)) {
+				reader.Read(iv, 0, IVSize);
+				aes.IV = iv;
+			}
+		}
+		
+		
+		//ripped from GenKeys to confirm if it gets the info right
+		using (FileStream stream = File.Create($"{userDir}\\loadIV.dat")) {
+			using (BinaryWriter writer = new BinaryWriter(stream)) {
+				writer.Write(aes.IV);
 			}
 		}
 		
 	}
 }
-*/
+
 GenKeys("testUser");
+HashPassword("testUser", "password");
